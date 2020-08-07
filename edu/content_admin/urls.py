864793ac1b018546d081fdb8a27d2a5fd0d40f4f
Eugane Lebedev <adm1n.lebedev@ya.ru>
2020-08-07 19:03:00 +0300

    delete view test include parts of html

--- edu/content_admin/urls.py
+++ edu/content_admin/urls.py
@@ -5,5 +5,7 @@ app_name = 'content_admin'
 
 urlpatterns = [
     path('news/create/', views.NewsCreateView.as_view(), name='news_create'),
-    path('news/<int:pk>/update/', views.NewsUpdateView.as_view(), name='news_update')
+    path('news/<int:pk>/update/', views.NewsUpdateView.as_view(), name='news_update'),
+    path('news/<int:pk>/delete', views.NewsDeleteView.as_view(), name = 'mews_delete'),
+
 ]
--- edu/content_admin/views.py
+++ edu/content_admin/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from index.models import NewsModel
 
 
@@ -14,3 +14,8 @@ class NewsUpdateView(UpdateView):
     model = NewsModel
     fields = ['title', 'title_image', 'content']
     template_name = 'content_admin/news_create.html'
+
+class NewsDeleteView(DeleteView):
+    model = NewsModel
+    template_name = 'content_admin/news_delete.html'
+
