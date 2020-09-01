f4f895786b2bf5e970a9f720dd6ed522aa3c8482
Eugane Lebedev <adm1n.lebedev@ya.ru>
2020-09-01 18:06:16 +0300

    Check results in test

--- edu/edu/courses/views.py
+++ edu/edu/courses/views.py
@@ -30,15 +30,27 @@ class ModuleTestDetailView(DetailView):
     context_object_name = 'test'
 
     def get(self, request, *args, **kwargs):
+        #TODO
+        # 1. Работа с несколькими правильными ответами
+        # 2. Привязка к пользователю
+        # 3. Проверить был ли уже ответ и вернуть предупреждение
+        # 4. Сделать форму с ответом неактивной после отправки запроса
+        # 5.
 
         if request.is_ajax():
-            text = request.GET.get('button_text')
-            print('IS AJAX')
-            print(text)
-            print(request.GET.get('my_answer_list'))
-            answer_list = json.loads(request.GET.get('my_answer_list'))
-            print('Answer', answer_list)
-            return JsonResponse({'seconds': 1}, status=200)
+            print(request.GET.get('my_answer'))
+            answer_list_deserialized = request.GET.get('my_answer')
+            return_answers = {}
+            if answer_list_deserialized:
+                answer = json.loads(answer_list_deserialized)
+                print(f'{answer=}', type(answer))
+                answer_model = models.Answer.objects.get(id=answer.get('answer_id'))
+                print(f'{answer_model.is_correct=}')
+                return_answers.update({'answer_id': answer_model.id, 'is_correct': answer_model.is_correct})
+
+            else:
+                print('wrong answer_list variable')
+            return JsonResponse({'seconds': 1, 'checked_answer': return_answers}, status=200)
 
         return super().get(request, *args, **kwargs)
 
--- edu/edu/static/js/main.js
+++ edu/edu/static/js/main.js
@@ -4,16 +4,18 @@ $(document).ready(function(){
         //window.alert($(this).parent().prop('nodeName'))
         let clickedItem = $(this);
         //let my_answer = {};
-        let answer_list = []
+        let my_answer = {};
         $(clickedItem.parent().children('ul').children().children('div')).each(function() {
+            if ($(this).children('input[name="answer_radio"]').is(':checked')) {
+                my_answer.question_id = $(this).children('input[name="question_id"]').prop('value')[0];
+                my_answer.answer_id = $(this).children('input[name="answer_radio"]:checked').val();
+                //console.log($(this).children('input[name="question_id"]').prop('value'));
+                //$(this).children('input').prop('nodeName') ;
+                //my_list += $(this).children('input[name="question_id"]').prop('nodeName') ;
+                //my_list += ':' + $(this).children('label').prop('nodeName') + '\n';
+            } else {
+            }
 
-            let my_answer = {};
-            my_answer.question_id = $(this).children('input[name="question_id"]').prop('value')[0];
-            console.log($(this).children('input[name="question_id"]').prop('value'));
-            //$(this).children('input').prop('nodeName') ;
-            //my_list += $(this).children('input[name="question_id"]').prop('nodeName') ;
-            //my_list += ':' + $(this).children('label').prop('nodeName') + '\n';
-            answer_list.push(my_answer);
 
 
         });
@@ -22,15 +24,26 @@ $(document).ready(function(){
             type: 'get',
             data: {
                 button_text: $(this).text(),
-                my_answer_list: JSON.stringify(answer_list)
+                my_answer: JSON.stringify(my_answer)
             },
             success: function(response) {
                 $(this).text(response.seconds)
                 $(".answer").toggleClass('btn-success')
-                $(".answer").toggleClass('btn-info')
+                $(".answer").toggleType('hidden')
                 clickedItem.parent().toggleClass('bg-warning')
                 //window.alert(clickedItem.parent().prop('nodeName'))
-                $('#seconds').append('<li>' + response.seconds + ' ' + answer_list  + '</li>')
+                $('#seconds').append('<li>1' + JSON.stringify(response.checked_answer)  + '</li>')
+                if (response.checked_answer.is_correct) {
+                    console.log('+')
+                    clickedItem.parent().append('<h1><div class="alert alert-success" role="alert">Правильный ответ</div></h1>')
+                }
+                else {
+                    console.log('-')
+                    clickedItem.parent().append('<h1><div class="alert alert-warning" role="alert">Ошибка</div></h1>')
+                }
+
+                answer_list = []
+                my_answer = {}
             }
         });
     });
